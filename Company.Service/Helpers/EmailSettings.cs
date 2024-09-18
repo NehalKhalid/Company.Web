b70711ba88e9d37255e77647065b2322f5d7c61a
Nehal <nehalkha92@gmail.com>
2024-09-18 20:45:33 +0200

    CommitFour

---
+++ Company.Service/Helpers/EmailSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Service.Helpers
+{
+    public static class EmailSettings
+    {
+        public static void SendEmail(Email input)
+        {
+            var client = new SmtpClient("smtp.gmail.com", 587);
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential("nehalkha92@gmail.com", "ajgzsjqcmuejokoi");
+            client.Send("nehalkha92@gmail.com",input.To, input.Subject, input.Body);
+
+        }
+    }
+}
---
+++ Company.Web/Models/ResetPasswordViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.Web.Models
+{
+	public class ResetPasswordViewModel
+	{
+		[Required(ErrorMessage = "Password Is Required !!")]
+		[RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$", ErrorMessage = "Password must be at least 6 characters long, contain at least one lowercase letter, one uppercase letter, one digit, and one non-alphanumeric character.")]
+		public string Password { get; set; }
+		[Required(ErrorMessage = "ConfirmPassword Is Required !!")]
+		[Compare(nameof(Password), ErrorMessage = "Confirm Password dose not Match Password !!")]
+		public string ConfirmPassword { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+    }
+}
