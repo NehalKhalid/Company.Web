26540194b9e00ac87d87429f22be4aade01ef42f
Nehal <nehalkha92@gmail.com>
2024-09-22 03:55:15 +0200

    CommitFive

---
+++ Company.Web/Controllers/RoleController.cs
@@ -0,0 +1,147 @@
+﻿using Company.Data.Models;
+using Company.Service.Services.Department.Dto;
+using Company.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Company.Web.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ILogger<RoleController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RoleController(RoleManager<IdentityRole> roleManager
+            ,ILogger<RoleController> logger
+            ,UserManager<ApplicationUser> userManager)
+        {
+            _roleManager = roleManager;
+            _logger = logger;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            return View(roles);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(IdentityRole identityRole)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _roleManager.CreateAsync(identityRole);
+                if(result.Succeeded)
+                    return RedirectToAction("Index");
+                foreach (var item in result.Errors)
+                    _logger.LogError(item.Description);
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Details(string? id, string viewName = "Details")
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role is null)
+                return NotFound();
+
+            if (viewName == "Update")
+            {
+                var roleViewModel = new UpdateRoleViewModel
+                {
+                    Id = role.Id,
+                    Name = role.Name
+                };
+                return View(viewName, roleViewModel);
+            }
+            return View(viewName, role);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Update(string? id)
+        {
+            return await Details(id, "Update");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(string? id, UpdateRoleViewModel roleViewModel)
+        {
+            if (roleViewModel.Id != id)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var role = await _roleManager.FindByIdAsync(id);
+                    if (role is null)
+                        return NotFound();
+                    role.Name = roleViewModel.Name;
+                    role.NormalizedName = roleViewModel.Name.ToUpper();
+                    var result = await _roleManager.UpdateAsync(role);
+                    if (result.Succeeded)
+                    {
+                        _logger.LogInformation("Role Updated Successfully");
+                        return RedirectToAction("Index");
+                    }
+
+                    foreach (var item in result.Errors)
+                        _logger.LogError(item.Description);
+
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex.Message);
+                }
+            }
+            return View(roleViewModel);
+        }
+
+        public async Task<IActionResult> Delete(string? id)
+        {
+            try
+            {
+                var role = await _roleManager.FindByIdAsync(id);
+                if (role is null)
+                    return NotFound();
+                var result = await _roleManager.DeleteAsync(role);
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                foreach (var item in result.Errors)
+                    _logger.LogError(item.Description);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> AddOrRemoveUsers(string roleId)
+        {
+            var role = await _roleManager.FindByIdAsync(roleId);
+            if(role is null)
+                return NotFound();
+            var users = await _userManager.Users.ToListAsync();
+            var usersInRole = new List<UserInRoleViewModel>();
+            foreach (var user in users)
+            {
+                var userInRole = new UserInRoleViewModel
+                {
+                    UserId = user.Id,
+                    Name = user.UserName
+                };
+                if (await _userManager.IsInRoleAsync(user, role.Name))
+                    userInRole.IsSelected = true;
+                else
+                    userInRole.IsSelected = false;
+                usersInRole.Add(userInRole);
+            }
+            return View(usersInRole);
+        }
+    }
+}
---
+++ Company.Web/Models/UserInRoleViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Company.Web.Models
+{
+    public class UserInRoleViewModel
+    {
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
